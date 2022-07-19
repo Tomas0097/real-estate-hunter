e35fc843db192254d88b95b9b19fb88ebe90f7cb
Tomas Rymon <tomas.rymon@gmail.com>
2022-07-19 23:41:03 +0200

    Adds new model: 'Marketplace'

--- src/web/models.py
+++ src/web/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from real_estate_hunter.models import IdModel
+
+
+class Marketplace(IdModel):
+    name = models.CharField(
+        verbose_name=_("Name"),
+        max_length=40,
+        blank=False,
+        null=False,
+    )
+    homepage_link = models.URLField(
+        verbose_name=_("Homepage link"),
+        blank=False,
+        null=False,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Marketplace")
+        verbose_name_plural = _("Marketplaces")
