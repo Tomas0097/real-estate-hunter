4ebafce2236dbffb59f0c757fe9dfc0f6c0eae1c
Tomas Rymon <tomas.rymon@gmail.com>
2022-07-21 17:04:14 +0200

    Adds MarkeplaceSourceLinkAdmin

--- src/web/admin.py
+++ src/web/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from web.models import Marketplace
+from web.models import Marketplace, MarketplaceSourceLink
 
 
 @admin.register(Marketplace)
 class MarketplaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MarketplaceSourceLink)
+class MarkeplaceSourceLinkAdmin(admin.ModelAdmin):
     pass
